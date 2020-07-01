ddec018ad1226f1ee85677c5c6b9487efa903cf5
Phillip Choi <jaeja-phil@gmail.com>
2020-07-01 17:42:24 +0900

    login with jwt complete

--- server/app.js
+++ server/app.js
@@ -1,18 +1,82 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
+const users = require("./model/users");
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("testing");
 });
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  users
+    .findOne({
+      where: {
+        email,
+        password,
+      },
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(403).send("result not found");
+      }
+      const user = result.dataValues.id;
+      jwt.sign({ user }, "secretKey", (err, token) => {
+        if (err) {
+          return res.status(403).send(err);
+        }
+        res.cookie("token", token);
+        res.send({
+          token,
+          email,
+        });
+      });
+    });
+});
 
-console.log(process.env.PASSWORD);
+app.get("/user", checkToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        msg: "login successful",
+        authData,
+      });
+    }
+  });
+});
+
+app.get("logout", (req, res) => {});
+
+function checkToken(req, res, next) {
+  const header = req.headers["authorization"];
+
+  if (typeof header !== "undefined") {
+    const bearer = header.split(" ");
+    const token = bearer[1];
+
+    req.token = token;
+    next();
+  } else {
+    //If header is undefined return Forbidden (403)
+    res.sendStatus(403);
+  }
+}
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- server/model/index.js
+++ server/model/index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // const fs = require("fs");
-=======
->>>>>>> 4456355b1121dec15835095d6f0f0221e5716671
 const path = require("path");
 const dotenv = require("dotenv");
 const { Sequelize, DataTypes } = require("sequelize");
@@ -18,50 +15,4 @@ let sequelize = new Sequelize(
 );
 
 sequelize.sync();
-<<<<<<< HEAD
-
-const users = sequelize.define(
-  "users",
-  {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    height: DataTypes.INTEGER,
-    weight: DataTypes.INTEGER,
-  },
-  {
-    charset: "utf8",
-    collate: "utf8_unicode_ci",
-  }
-);
-
-const foods = sequelize.define(
-  "foods",
-  {
-    user_id: DataTypes.STRING,
-    food_name: DataTypes.STRING,
-    date: DataTypes.DATE,
-    calories: DataTypes.FLOAT,
-    fat: DataTypes.FLOAT,
-    carbohydrates: DataTypes.FLOAT,
-    sugar: DataTypes.FLOAT,
-    protein: DataTypes.FLOAT,
-    sodium: DataTypes.FLOAT,
-    cholesterol: DataTypes.FLOAT,
-    iron: DataTypes.FLOAT,
-    calcium: DataTypes.FLOAT,
-    vitamin_A: DataTypes.FLOAT,
-    vitamin_D: DataTypes.FLOAT,
-    zinc: DataTypes.FLOAT,
-  },
-  {
-    charset: "utf8",
-    collate: "utf8_unicode_ci",
-  }
-);
-
-// module.exports = db;
-=======
 module.exports = { sequelize, DataTypes };
->>>>>>> 4456355b1121dec15835095d6f0f0221e5716671
