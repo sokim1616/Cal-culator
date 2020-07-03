63cf3c0d0ac7725db25213428e24d186a1e4a023
Macintosh HD <sokim1616@gmail.com>
2020-07-04 00:04:41 +0900

    Created Dodont chart & Modified Chart-monthly

--- client/src/component/Dodont/Dodont.js
+++ client/src/component/Dodont/Dodont.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '@rmwc/data-table/styles';
+import '@rmwc/list/styles';
 // 해당하는 부분 import 해오기
-import { SimpleDataTable } from '@rmwc/data-table';
+import { List, CollapsibleList, SimpleListItem } from '@rmwc/list';
 import './Dodont.css'
 
 
@@ -12,80 +12,125 @@ const DoDont = () => {
     //const [] = React.useState();
 
     return (
-        <div>
-            <span className='whattoeat'>
-                <header className='header1'>What to eat</header>
-                <SimpleDataTable
-                    getRowProps={row => {
-                        return row[1] > 100 ? { activated: true } : {};
-                    }}
-                    getCellProps={(cell, index, isHead) => {
-                        const props = { isNumeric: index > 0, style: undefined };
+        <List>
+            <CollapsibleList
+                handle={
+                    <SimpleListItem
+                        text="What to eat"
+                    // graphic="favorite"
+                    // metaIcon="chevron_right"
+                    />
+                }
+                onOpen={() => console.log('open')}
+                onClose={() => console.log('close')}
+            >
+                <SimpleListItem text="Water" />
+                <SimpleListItem text="Avocado" />
+                <SimpleListItem text="Olives" />
+            </CollapsibleList>
 
-                        return !isHead && index === 2 && !cell.includes('$')
-                            ? { ...props, style: { color: 'red' } }
-                            : props;
-                    }}
-                    headers={[['Item', 'Quantity', 'Value']]}
-                    data={[
-                        ['Cookies', 25, '$12.40'],
-                        ['Pizza', 11, '$10.43'],
-                        ['Icecream', 3, '1.43'],
-                        ['Candy', 72, '$22.45'],
-                        ['Cakes', 101, '$215.05'],
-                        ['Muffins', 3, '$5.97']
-                    ]}
-                />
-            </span>
-            <div className='whatnottoeat'>
-                <header className='header1'>What NOT to eat</header>
-                <SimpleDataTable
-                    getRowProps={row => {
-                        return row[1] > 100 ? { activated: true } : {};
-                    }}
-                    getCellProps={(cell, index, isHead) => {
-                        const props = { isNumeric: index > 0, style: undefined };
+            <CollapsibleList
+                handle={
+                    <SimpleListItem
+                        text="What NOT to eat"
+                    // graphic="local_pizza"
+                    // metaIcon="chevron_right"
+                    />
+                }
+            >
+                <SimpleListItem text="Flour" />
+                <SimpleListItem text="Pizza" />
+                <SimpleListItem text="Coke" />
+            </CollapsibleList>
 
-                        return !isHead && index === 2 && !cell.includes('$')
-                            ? { ...props, style: { color: 'red' } }
-                            : props;
-                    }}
-                    headers={[['Item', 'Quantity', 'Value']]}
-                    data={[
-                        ['Cookies', 25, '$12.40'],
-                        ['Pizza', 11, '$10.43'],
-                        ['Icecream', 3, '1.43'],
-                        ['Candy', 72, '$22.45'],
-                        ['Cakes', 101, '$215.05'],
-                        ['Muffins', 3, '$5.97']
-                    ]}
-                />
-            </div>
-            <div className='whatnottoeat'>
-                <header className='header1'>What NOT to eat</header>
-                <SimpleDataTable
-                    getRowProps={row => {
-                        return row[1] > 100 ? { activated: true } : {};
-                    }}
-                    getCellProps={(cell, index, isHead) => {
-                        const props = { isNumeric: index > 0, style: undefined };
+            <CollapsibleList
+                handle={
+                    <SimpleListItem
+                        text="What to do"
+                    // graphic="star"
+                    // metaIcon="chevron_right"
+                    />
+                }
+            >
+                <SimpleListItem text="Walk 20 mins a day" />
+                <SimpleListItem text="Squat 200 times a day" />
+            </CollapsibleList>
+        </List>
 
-                        return !isHead && index === 2 && !cell.includes('$')
-                            ? { ...props, style: { color: 'red' } }
-                            : props;
-                    }}
-                    headers={[['Item', 'Quantity', 'Value']]}
-                    data={[
-                        ['Cookies', 25, '$12.40'],
-                        ['Pizza', 11, '$10.43'],
-                        ['Icecream', 3, '1.43'],
-                        ['Candy', 72, '$22.45'],
-                        ['Cakes', 101, '$215.05'],
-                        ['Muffins', 3, '$5.97']
-                    ]}
-                />
-            </div>
-        </div>
+        // <div>
+        //     <div className='whattoeat'>
+        //         <header className='header1'>What to eat</header>
+        //         <SimpleDataTable
+        //             getRowProps={row => {
+        //                 return row[1] > 100 ? { activated: true } : {};
+        //             }}
+        //             getCellProps={(cell, index, isHead) => {
+        //                 const props = { isNumeric: index > 0, style: undefined };
+
+        //                 return !isHead && index === 2 && !cell.includes('$')
+        //                     ? { ...props, style: { color: 'red' } }
+        //                     : props;
+        //             }}
+        //             headers={[['Nutrition in Need', 'Food to Consume']]}
+        //             data={[
+        //                 ['Cookies', '$12.40'],
+        //                 ['Pizza', '$10.43'],
+        //                 ['Icecream', '1.43'],
+        //                 ['Candy', '$22.45'],
+        //                 ['Cakes', '$215.05'],
+        //                 ['Muffins', '$5.97']
+        //             ]}
+        //         />
+        //     </div>
+        //     <div className='whatnottoeat'>
+        //         <header className='header1'>What NOT to eat</header>
+        //         <SimpleDataTable
+        //             getRowProps={row => {
+        //                 return row[1] > 100 ? { activated: true } : {};
+        //             }}
+        //             getCellProps={(cell, index, isHead) => {
+        //                 const props = { isNumeric: index > 0, style: undefined };
+
+        //                 return !isHead && index === 2 && !cell.includes('$')
+        //                     ? { ...props, style: { color: 'red' } }
+        //                     : props;
+        //             }}
+        //             headers={[['Exceeding Nutrition', 'Food to Avoid']]}
+        //             data={[
+        //                 ['Cookies', '$12.40'],
+        //                 ['Pizza', '$10.43'],
+        //                 ['Icecream', '1.43'],
+        //                 ['Candy', '$22.45'],
+        //                 ['Cakes', '$215.05'],
+        //                 ['Muffins', '$5.97']
+        //             ]}
+        //         />
+        //     </div>
+        //     <div className='whattodo'>
+        //         <header className='header1'>What to do</header>
+        //         <SimpleDataTable
+        //             getRowProps={row => {
+        //                 return row[1] > 100 ? { activated: true } : {};
+        //             }}
+        //             getCellProps={(cell, index, isHead) => {
+        //                 const props = { isNumeric: index > 0, style: undefined };
+
+        //                 return !isHead && index === 2 && !cell.includes('$')
+        //                     ? { ...props, style: { color: 'red' } }
+        //                     : props;
+        //             }}
+        //             headers={[['Level of your health', 'Exercise to be done']]}
+        //             data={[
+        //                 ['Cookies', 25, '$12.40'],
+        //                 ['Pizza', 11, '$10.43'],
+        //                 ['Icecream', 3, '1.43'],
+        //                 ['Candy', 72, '$22.45'],
+        //                 ['Cakes', 101, '$215.05'],
+        //                 ['Muffins', 3, '$5.97']
+        //             ]}
+        //         />
+        //     </div>
+        // </div>
     )
 }
 
