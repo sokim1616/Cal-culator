5c2ad703ec5cd00e916d2e521433e2950c6791d9
Phillip Choi <jaeja-phil@gmail.com>
2020-07-03 10:23:03 +0900

    starting day server, adding infoDay

--- server/controller/user/user_info_day.js
+++ server/controller/user/user_info_day.js
@@ -5,8 +5,15 @@ module.exports = {
   post: (req, res) => {
     const id = req.session.userid;
     const { day } = req.body;
-    const startDay = day + "T00:00:00Z";
-    const endDay = day + "T23:59:59Z";
+    // if the day is in the format of 2020-07-01
+    // const startDay = day + "T00:00:00Z";
+    // const endDay = day + "T23:59:59Z";
+    // if the day is in the format of Date()
+    const formattedDay = `${day.getFullYear()}-${
+      day.getMonth() + 1 > 9 ? day.getMonth() + 1 : "0" + (day.getMonth() + 1)
+    }-${day.getDate() > 9 ? day.getDate() : "0" + day.getDate()}`;
+    const startDay = formattedDay + "T00:00:00Z";
+    const endDay = formattedDay + "T23:59:59Z";
     Food_user.findAll({
       where: {
         UserId: 1,
