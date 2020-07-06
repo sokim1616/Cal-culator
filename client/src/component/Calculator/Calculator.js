c9b3f96ba027c7c2e806e834316fd94da403166c
JunSeobKim <wnstjq616@gmail.com>
2020-07-06 12:31:47 +0900

    calculator css

--- client/src/component/Calculator/Calculator.js
+++ client/src/component/Calculator/Calculator.js
@@ -2,26 +2,27 @@ import React from "react";
 import Search from "./calculator-search";
 import FoodList from "./calculator-foodlist";
 import Cart from "./calculator-cart";
+import "./Calculator.css";
 
 const Calculator = () => {
   const [searchResult, setSearchResult] = React.useState({
-    "id": 1,
-    "food_name": "pizza",
-    "image": "https://spoonacular.com/recipeImages/210327-312x231.jpg",
-    "calories": 363.65,
-    "fat": 13.08,
-    "carbohydrates": 51.83,
-    "sugar": 2.38,
-    "protein": 9.71,
-    "sodium": 14.76,
-    "cholesterol": 5.04,
-    "iron": 3.17,
-    "calcium": 69.59,
-    "vitamin_A": 359.53,
-    "vitamin_D": 0,
-    "zinc": 0.71,
-    "createdAt": "2020-07-03T02:20:17.000Z",
-    "updatedAt": "2020-07-03T02:20:17.000Z"
+    id: 1,
+    food_name: "pizza",
+    image: "https://spoonacular.com/recipeImages/210327-312x231.jpg",
+    calories: 363.65,
+    fat: 13.08,
+    carbohydrates: 51.83,
+    sugar: 2.38,
+    protein: 9.71,
+    sodium: 14.76,
+    cholesterol: 5.04,
+    iron: 3.17,
+    calcium: 69.59,
+    vitamin_A: 359.53,
+    vitamin_D: 0,
+    zinc: 0.71,
+    createdAt: "2020-07-03T02:20:17.000Z",
+    updatedAt: "2020-07-03T02:20:17.000Z",
   });
   const [searchInput, setSearchInput] = React.useState();
   const [startDate, setStartDate] = React.useState();
@@ -36,8 +37,8 @@ const Calculator = () => {
   };
 
   const addDateHandle = (e) => {
-    setStartDate(e)
-  }
+    setStartDate(e);
+  };
 
   const addToCartButton = () => {
     setResultSave((prevState) => [
@@ -46,13 +47,13 @@ const Calculator = () => {
         id: searchResult.id,
         date: startDate,
         foodname: searchResult.food_name,
-        calories: searchResult.calories
-      }
-    ])
-  }
+        calories: searchResult.calories,
+      },
+    ]);
+  };
 
   return (
-    <div>
+    <div className="calculator">
       <div>
         <Search
           searchInputHandle={searchInputHandle}
@@ -60,18 +61,22 @@ const Calculator = () => {
           searchInput={searchInput}
         />
       </div>
-      <div className="food">
-        <FoodList
-          searchResult={searchResult}
-          addDateHandle={addDateHandle}
-          addToCartButton={addToCartButton} />
-      </div>
-      <div className="cart">
-        <Cart
-          searchResult={searchResult}
-          startDate={startDate}
-          resultSave={resultSave}
-          setResultSave={setResultSave} />
+      <div className="food-cart">
+        <div className="food">
+          <FoodList
+            searchResult={searchResult}
+            addDateHandle={addDateHandle}
+            addToCartButton={addToCartButton}
+          />
+        </div>
+        <div className="cart">
+          <Cart
+            searchResult={searchResult}
+            startDate={startDate}
+            resultSave={resultSave}
+            setResultSave={setResultSave}
+          />
+        </div>
       </div>
     </div>
   );
--- client/src/component/Calculator/calculator-cart.js
+++ client/src/component/Calculator/calculator-cart.js
@@ -1,5 +1,6 @@
 import React from "react";
-import AddCart from './calculator-add-cart'
+import AddCart from "./calculator-add-cart";
+import "./Calculator.css";
 
 import "@rmwc/data-table/styles";
 import {
@@ -29,36 +30,37 @@ import "@rmwc/dialog/styles";
 // import stcp from "styled-components"
 
 const Cart = ({ searchResult, startDate, resultSave, setResultSave }) => {
-
   const [deleteOpen, setDeleteOpen] = React.useState(false);
   const [confirmOpen, setConfirmOpen] = React.useState(false);
-  const [checked, setChecked] = React.useState({0:false});
+  const [checked, setChecked] = React.useState({ 0: false });
 
   const checkedHandle = () => {
-    setChecked({ ...checked })
-  }
+    setChecked({ ...checked });
+  };
 
   const deleteButtonHandle = () => {
     for (let key in checked) {
       if (checked[key] === true) {
-        setChecked({[key]:false})
-        setResultSave(resultSave.filter((ele, idx) => ele.key !== key))
+        setChecked({ [key]: false });
+        setResultSave(resultSave.filter((ele, idx) => ele.key !== key));
       }
     }
-  }
+  };
 
-  const confirmButtonHandle = () => {
-    
-  }
+  const confirmButtonHandle = () => {};
 
   // const DataTable = stcp(DataTableHead)`border: 1px solid black`;
 
   return (
-    <div>
-      <div>
-        <DataTable className="cart-table" style={{ width: "800px" }}>
+    <>
+      <h1>CART</h1>
+      <div className="cart--foodlist">
+        <DataTable
+          className="cart-table"
+          style={{ height: "550px", width: "100%" }}
+        >
           <DataTableContent>
-            <DataTableHead >
+            <DataTableHead>
               <DataTableRow>
                 <DataTableHeadCell hasFormControl></DataTableHeadCell>
                 <DataTableHeadCell>Date</DataTableHeadCell>
@@ -73,63 +75,75 @@ const Cart = ({ searchResult, startDate, resultSave, setResultSave }) => {
                 checkedHandle={checkedHandle}
                 searchResult={searchResult}
                 startDate={startDate}
-                resultSave={resultSave} />
+                resultSave={resultSave}
+              />
             </DataTableBody>
           </DataTableContent>
         </DataTable>
       </div>
-      <div className="total-calorie">
-        <SimpleDataTable data={[["Total Calorie", "3000 kcal"]]} />
-      </div>
-      <div className="cart-button">
-        <span>
-          <Dialog
-            open={deleteOpen}
-            onClose={evt => {
-              console.log(evt.detail.action);
-              setDeleteOpen(false);
-            }}
-            onClosed={evt => console.log(evt.detail.action)}
-          >
-            <DialogTitle>Delete</DialogTitle>
-            <DialogContent>Do you really want to delete the selected item(s)...? really...?</DialogContent>
-            <DialogActions>
-              <DialogButton action="close">Cancel</DialogButton>
-              <DialogButton onClick={deleteButtonHandle} action="accept" isDefaultAction>
-                Delete
-      </DialogButton>
-            </DialogActions>
-          </Dialog>
+      <div className="cart--confirm">
+        <div className="total-calorie">
+          <SimpleDataTable data={[["Total Calorie", "3000 kcal"]]} />
+        </div>
+        <div className="cart-button">
+          <div className="cart-button__delete">
+            <Dialog
+              open={deleteOpen}
+              onClose={(evt) => {
+                console.log(evt.detail.action);
+                setDeleteOpen(false);
+              }}
+              onClosed={(evt) => console.log(evt.detail.action)}
+            >
+              <DialogTitle>Delete</DialogTitle>
+              <DialogContent>
+                Do you really want to delete the selected item(s)...? really...?
+              </DialogContent>
+              <DialogActions>
+                <DialogButton action="close">Cancel</DialogButton>
+                <DialogButton
+                  onClick={deleteButtonHandle}
+                  action="accept"
+                  isDefaultAction
+                >
+                  Delete
+                </DialogButton>
+              </DialogActions>
+            </Dialog>
 
-          <Button raised onClick={() => setDeleteOpen(true)}>
-            DELETE
-  </Button>
-        </span>
-        <span>
-          <Dialog
-            open={confirmOpen}
-            onClose={evt => {
-              console.log(evt.detail.action);
-              setConfirmOpen(false);
-            }}
-            onClosed={evt => console.log(evt.detail.action)}
-          >
-            <DialogTitle>Confirm</DialogTitle>
-            <DialogContent>Do you really want to confirm the selected item(s)...? really...?</DialogContent>
-            <DialogActions>
-              <DialogButton action="close">Cancel</DialogButton>
-              <DialogButton action="accept" isDefaultAction>
-                Confirm
-          </DialogButton>
-            </DialogActions>
-          </Dialog>
+            <Button raised onClick={() => setDeleteOpen(true)}>
+              DELETE
+            </Button>
+          </div>
+          <div>
+            <Dialog
+              open={confirmOpen}
+              onClose={(evt) => {
+                console.log(evt.detail.action);
+                setConfirmOpen(false);
+              }}
+              onClosed={(evt) => console.log(evt.detail.action)}
+            >
+              <DialogTitle>Confirm</DialogTitle>
+              <DialogContent>
+                Do you really want to confirm the selected item(s)...?
+                really...?
+              </DialogContent>
+              <DialogActions>
+                <DialogButton action="close">Cancel</DialogButton>
+                <DialogButton action="accept" isDefaultAction>
+                  Confirm
+                </DialogButton>
+              </DialogActions>
+            </Dialog>
 
-          <Button raised onClick={() => setConfirmOpen(true)}>
-            CONFIRM
-      </Button>
-        </span>
+            <Button raised onClick={() => setConfirmOpen(true)}>
+              CONFIRM
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
