c3faad138dcdc128f1b9478962fce84f5acf1b56
Phillip Choi <jaeja-phil@gmail.com>
2020-07-07 15:07:14 +0900

    caclculator downloaded img for the nutrients

--- client/src/component/Dodont/Dodont.js
+++ client/src/component/Dodont/Dodont.js
@@ -10,8 +10,8 @@ const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 
 const DoDont = ({ setCurrentPageIndex }) => {
+  const [date] = useState(formatDay(yesterday));
   // const [date] = useState(formatDay(yesterday));
-  const [date] = useState("2020-07-01");
   const [whatToEat, setWhatToEat] = useState([]);
   const [whatNotToEat, setWhatNotToEat] = useState([]);
 
@@ -19,63 +19,88 @@ const DoDont = ({ setCurrentPageIndex }) => {
     setCurrentPageIndex(4);
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .post(
-  //       "http://localhost:4000/user/infoDay",
-  //       { date },
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-type": "application/json",
-  //         },
-  //         withCredentials: true,
-  //       }
-  //     )
-  //     .then((result) => {
-  //       if (typeof requiredNutrients(result.data) === "string") {
-  //         setWhatToEat(["Aren't you hungry? Eat whatever you want!"]);
-  //       } else {
-  //         const threeNonNeededNutrients = requiredNutrients(
-  //           result.data,
-  //           "dontNeed"
-  //         );
-  //         const threeNeededNutrients = requiredNutrients(result.data, "inNeed");
+  useEffect(() => {
+    axios
+      .post(
+        "http://localhost:4000/user/infoDay",
+        { date },
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((result) => {
+        if (typeof requiredNutrients(result.data) === "string") {
+          setWhatToEat([
+            {
+              title: "Aren't you hungry? Eat whatever you want!",
+              image: `baby.jpg`,
+            },
+          ]);
+          setWhatNotToEat([
+            {
+              title: "Probably not these...",
+              image: `veges.jpg`,
+            },
+          ]);
+        } else {
+          const threeNonNeededNutrients = requiredNutrients(
+            result.data,
+            "dontNeed"
+          );
+          const threeNeededNutrients = requiredNutrients(result.data, "inNeed");
 
-  //         threeNonNeededNutrients.forEach((nutrient) => {
-  //           axios
-  //             .get("http://localhost:4000/food/findmaxnutrient", {
-  //               params: {
-  //                 nutrient: nutrient[0],
-  //               },
-  //             })
-  //             .then((result) => {
-  //               setWhatNotToEat((prevState) => [...prevState, result.data]);
-  //             });
-  //         });
+          threeNonNeededNutrients.forEach((nutrient) => {
+            setWhatNotToEat((prevState) => [
+              ...prevState,
+              {
+                title: nutrient[0],
+                image: `${nutrient[0]}.jpg`,
+              },
+            ]);
+          });
+          threeNeededNutrients.forEach((nutrient) => {
+            setWhatToEat((prevState) => [
+              ...prevState,
+              {
+                title: nutrient[0],
+                image: `${nutrient[0]}.jpg`,
+              },
+            ]);
+          });
+          // threeNonNeededNutrients.forEach((nutrient) => {
+          //   axios
+          //     .get("http://localhost:4000/food/findmaxnutrient", {
+          //       params: {
+          //         nutrient: nutrient[0],
+          //       },
+          //     })
+          //     .then((result) => {
+          //       setWhatNotToEat((prevState) => [...prevState, result.data]);
+          //     });
+          // });
 
-  //         threeNeededNutrients.forEach((nutrient) => {
-  //           axios
-  //             .get("http://localhost:4000/food/findmaxnutrient", {
-  //               params: {
-  //                 nutrient: nutrient[0],
-  //               },
-  //             })
-  //             .then((result) => {
-  //               setWhatToEat((prevState) => [...prevState, result.data]);
-  //             });
-  //         });
-  //       }
-  //     });
-  // }, []);
+          // threeNeededNutrients.forEach((nutrient) => {
+          //   axios
+          //     .get("http://localhost:4000/food/findmaxnutrient", {
+          //       params: {
+          //         nutrient: nutrient[0],
+          //       },
+          //     })
+          //     .then((result) => {
+          //       setWhatToEat((prevState) => [...prevState, result.data]);
+          //     });
+          // });
+        }
+      });
+  }, []);
 
   return (
     <center>
       <List className='list'>
-        <div>
-          Click on each box to see what you are recommended to eat, to avoid
-          eating, and to do.
-        </div>
         <CollapsibleList
           handle={<SimpleListItem className='head1' text='What to eat' />}
           onOpen={() => console.log("open")}
@@ -85,7 +110,11 @@ const DoDont = ({ setCurrentPageIndex }) => {
             return (
               <React.Fragment key={idx}>
                 <SimpleListItem text={food.title} />
-                <img src={food.image} alt='foodImage' />
+                <img
+                  src={require(`./img/${food.image}`)}
+                  alt='foodImage'
+                  style={{ width: "50%", height: "50%" }}
+                />
               </React.Fragment>
             );
           })}
@@ -98,7 +127,11 @@ const DoDont = ({ setCurrentPageIndex }) => {
             return (
               <React.Fragment key={idx}>
                 <SimpleListItem text={food.title} />
-                <img src={food.image} alt='foodImage' />
+                <img
+                  src={require(`./img/${food.image}`)}
+                  alt='foodImage'
+                  style={{ width: "50%", height: "50%" }}
+                />
               </React.Fragment>
             );
           })}
