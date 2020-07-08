b861dbb0c4841280a2499433c0a3dad3a715a895
경두현 <douhyun.kyung@gmail.com>
2020-07-08 23:04:59 +0900

    login/signup completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Router, Redirect } from "react-router-dom";
+import Modal from 'react-modal';
+import axios from 'axios';
 import Home from "./component/Home/Home";
 import Signup from "./component/Signup";
 import Summary from "./component/Summary/Summary";
@@ -10,11 +12,20 @@ import Calculator from "./component/Calculator/Calculator";
 import DoDont from "./component/Dodont/Dodont";
 import About from "./component/About/About";
 
+Modal.setAppElement('#root')
+
 const App = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [loginModalOpen, setLoginModalOpen] = useState(false);
   const [signupModalOpen, setSignupModalOpen] = useState(false);
   const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  const [trivia, setTrivia] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/food/foodtrivia").then((result) => {
+      setTrivia(result.data);
+    });
+  }, []);
 
   const loginState = () => {
     setIsLogin(true);
@@ -68,7 +79,6 @@ const App = () => {
             path='/about'
             render={() => <About
               setCurrentPageIndex={setCurrentPageIndex}
-
             />}
           />
           <Route
@@ -85,6 +95,7 @@ const App = () => {
             render={() => (
               <Calculator
                 isLogin={isLogin}
+                trivia={trivia}
                 setCurrentPageIndex={setCurrentPageIndex}
               />
             )}
